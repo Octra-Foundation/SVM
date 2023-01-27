874a3cf5f18371a8573b448940a3a9a13d1228da
K <117524908+lambda0xE@users.noreply.github.com>
2023-01-27 22:15:50 +0000

    Create sigmoid.ml

---
+++ sigmoid.ml
@@ -0,0 +1,15 @@
+module Sigmoid = struct
+  type t = { logit_size : int; logit_table : float array }
+
+  let create logit_size =
+    let logit_table = Array.init logit_size (fun i -> log ((float (i + 1)) /. (float (logit_size + 1))) -. log (1. -. (float (i + 1)) /. (float (logit_size + 1)))) in
+    { logit_size; logit_table }
+
+  let logit { logit_size; logit_table } p =
+    let index = int_of_float (p *. float logit_size) in
+    if index >= logit_size then logit_table.(logit_size - 1)
+    else if index < 0 then logit_table.(0)
+    else logit_table.(index)
+
+  let logistic p = 1. /. (1. +. exp (-.p))
+end
