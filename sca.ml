96a913859cbb56fbc7b9a69ca6395edf42224a5f
K <117524908+lambda0xE@users.noreply.github.com>
2023-01-31 00:27:00 +0000

    Update sca.ml

--- sca.ml
+++ sca.ml
@@ -1,6 +1,6 @@
 let iteration_count = 1;;
 let constant_k = 1.0;; (* the constant k for the Spiral Computation Algorithm *)
-
+type pair = float * float
 let atan_lookup_table = Array.init iteration_count (fun i -> atan (2.0 ** (float (-i))));;
 
 let spiral_computation_algorithm input_angle =
@@ -17,3 +17,31 @@ let spiral_computation_algorithm input_angle =
     angle := new_angle;
   done;
   atan2 !y_coordinate !x_coordinate;;
+
+let rec sort_data_points lst =
+  match lst with
+  | [] -> []
+  | hd::tl ->
+    let left, right = List.partition (fun x -> fst x <= fst hd) tl in
+    sort_data_points left @ [hd] @ sort_data_points right
+
+let adaptive_polf x y =
+  let data = List.combine x y in
+  let sorted_data = sort_data_points data in
+  let rec get_slopes data_points acc =
+    match data_points with
+    | [] -> acc
+    | hd::[] -> acc
+    | (x1, y1)::(x2, y2)::tl ->
+      let slope = (y2 -. y1) /. (x2 -. x1) in
+      get_slopes ((x2, y2)::tl) (slope::acc)
+  in
+  let slopes = List.rev (get_slopes sorted_data []) in
+  let rec find_piecewise_linear_function x = function
+    | [] -> raise Not_found
+    | (x1, y1)::[] -> y1
+    | (x1, y1)::(x2, y2)::tl when x1 <= x && x <= x2 ->
+      y1 +. (x -. x1) *. (List.hd slopes)
+    | (x1, y1)::(x2, y2)::tl -> find_piecewise_linear_function x ((x2, y2)::tl)
+  in
+  find_piecewise_linear_function
