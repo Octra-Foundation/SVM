c6143ad2011d5d36c1e00375ae0ea8e87a378df3
K <117524908+lambda0xE@users.noreply.github.com>
2023-02-04 17:49:22 +0000

    Update svm_test.ml

--- svm_test.ml
+++ svm_test.ml
@@ -1,5 +1,6 @@
 open Svm
 open Lacaml.D
+open Printf
 
 let data = [((1., 0.), 1); ((0., 1.), -1); ((0., 0.), -1); ((1., 1.), 1)]
 
@@ -17,3 +18,15 @@ let classify point = Svm.predict svm point
 let classify_matrix m = Mat.init (Mat.dim2 m) classify m
 
 let result = classify_matrix x
+
+
+let f n = 
+  let sqrt_term = sqrt(16.0 ** (float_of_int n) +. 1.0) in
+  let inner_term = 2.0 *. 4.0 ** (float_of_int n) +. 2.0 *. sqrt_term in
+  1.0 +. sqrt(inner_term) /. 2.0 ** (2.0 *. (float_of_int n) +. 2.0)
+;;
+
+let () =
+  for i = 1 to 100000 do
+    printf "%f\n" (f i)
+  done
