6fb6d8a78291dbb6f968c59871b32a49becdc821
K <117524908+lambda0xE@users.noreply.github.com>
2023-02-05 22:03:28 +0000

    Update svm_test.ml

--- svm_test.ml
+++ svm_test.ml
@@ -19,14 +19,37 @@ let classify_matrix m = Mat.init (Mat.dim2 m) classify m
 
 let result = classify_matrix x
 
-
-let f n = 
+let f n =
   let sqrt_term = sqrt(16.0 ** (float_of_int n) +. 1.0) in
   let inner_term = 2.0 *. 4.0 ** (float_of_int n) +. 2.0 *. sqrt_term in
   1.0 +. sqrt(inner_term) /. 2.0 ** (2.0 *. (float_of_int n) +. 2.0)
 ;;
 
-let () =
-  for i = 1 to 100000 do
-    printf "%f\n" (f i)
-  done
+let sigmoid x = 1. /. (1. +. exp (-.x))
+
+let init_state () =
+  let m1 = Array.make_matrix 100 100 0. in
+  let m2 = Array.make_matrix 100 100 0. in
+  let m_res = Array.make_matrix 100 100 0. in
+  for i = 0 to 99 do
+    for j = 0 to 99 do
+      m1.(i).(j) <- f (i * 100 + j);
+      m2.(i).(j) <- sigmoid m1.(i).(j);
+    done
+  done;
+  for i = 0 to 99 do
+    for j = 0 to 99 do
+      for k = 0 to 99 do
+        m_res.(i).(j) <- m_res.(i).(j) +. m1.(i).(k) *. m2.(k).(j);
+      done
+    done
+  done;
+  m_res
+;;
+
+let print_float_matrix mat =
+  Array.iter (fun row -> Array.iter (Printf.printf "%f ") row; print_endline "") mat
+;;
+
+let result = init_state () in
+print_float_matrix result
