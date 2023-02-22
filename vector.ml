241bda1888bb901d69db632ed9a4b3c31c409693
K <117524908+lambda0xE@users.noreply.github.com>
2023-02-22 04:42:19 +0000

    Update vector.ml

--- vector.ml
+++ vector.ml
@@ -1,5 +1,3 @@
-(* SVM Bit Predictor *)
-
 let c_max_value = 65535
 let r_l_value = 512
 let r_s_value = 82
@@ -10,7 +8,6 @@ let count_value = ref 0
 let w_weight = ref 0
 
 let octra_vector y =
-  (* Update input sequence state *)
   s1_state := (!s1_state lsl 1) lor y;
   let k_value = (!count_value mod 2) + 2 * y in
   s2_state := (!s2_state lsl 1) * (k_value mod 2) + (!s2_state + (2 * k_value - 1)) * (k_value lsr 1);
@@ -22,10 +19,10 @@ let octra_vector y =
   let x3_value = (x2_value + (!s2_state land (n_values.(3) - 1))) land (n_values.(3) - 1) in
 
   let error = (y lsl 16) - (!w_weight lsr 16) in
-  w_weight := !w_weight + (error * (r_s_value + 2 * r_l_value * (x0_value + x1_value + x2_value + x3_value))) in
-if !w_weight > c_max_value || !w_weight < -c_max_value then
-  w_weight := (!w_weight asr 1) + (if !w_weight > 0 then 1 else -1);
+  w_weight := !w_weight + (error * (r_s_value + 2 * r_l_value * (x0_value + x1_value + x2_value + x3_value)));
+  if !w_weight > c_max_value || !w_weight < -c_max_value then
+    w_weight := (!w_weight asr 1) + (if !w_weight > 0 then 1 else -1);
 
-let exponent = float_of_int (!w_weight lsr 8) in
-let pr_value = 1.0 /. (1.0 +. exp (-1.0 *. exponent)) in
-pr_value
+  let exponent = float_of_int (!w_weight lsr 8) in
+  let pr_value = 1.0 /. (1.0 +. exp (-1.0 *. exponent)) in
+  pr_value ;;
